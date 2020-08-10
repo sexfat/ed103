5e03b181340b76c64b8acdfdba33b1743705fb6a
sexfat <bryant6831@gmail.com>
2020-08-10 11:55:15 +0800

    add stagger

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-
 //單一物件
 TweenMax.to('.box_00', 2, {
     //屬性
@@ -7,7 +6,7 @@ TweenMax.to('.box_00', 2, {
     // ease:Elastic.easeOut, //補間動畫
     // width : '200px',
     // height : '200px',
-    repeat : 1,
+    repeat: 1,
     yoyo: true
 
     //repeatDelay: 1
@@ -19,41 +18,56 @@ TweenMax.to('.box_00', 2, {
 // });
 
 //from  方向相反
-TweenMax.from('.from' , 10 , {
-   x: 400,
-   repeat : 2,
-   ease:Elastic.easeOut //補間動畫
+TweenMax.from('.from', 10, {
+    x: 400,
+    repeat: 2,
+    ease: Elastic.easeOut //補間動畫
 
 });
 
-TweenMax.to('.circle' , .8 , {
-   rotation: 360,
-   repeat: 1, //連續
-   ease : Power0.easeNone //旋轉等速度
+TweenMax.to('.circle', .8, {
+    rotation: 360,
+    repeat: 1, //連續
+    ease: Power0.easeNone //旋轉等速度
 });
 
 
 //縮放
-TweenMax.to('.scale' , 4 , {
+TweenMax.to('.scale', 4, {
     //縮放
-      scale: 10,
-      rotation: 360, // 旋轉
-      ease:Elastic.easeOut, //補間動畫
-      opacity : 1,
-      repeat: 1,
-      yoyo : true //收回
+    scale: 10,
+    rotation: 360, // 旋轉
+    ease: Elastic.easeOut, //補間動畫
+    opacity: 1,
+    repeat: 1,
+    yoyo: true //收回
     // scaleX: 3
     // scaleY: 3
 });
 
 
-TweenMax.to('.rotation' , 2 , {
+TweenMax.to('.rotation', 2, {
     rotationX: 360,
-    transformOrigin : '10 bottom 150', // bottom center top right left
+    transformOrigin: '10 bottom 150', // bottom center top right left
     transformPerspective: 1200,
     scale: 2
-})
+});
 
+
+TweenMax.fromTo('.fromto', 3, {
+    x: 100
+}, {
+    x: 300,
+    ease: Elastic.easeOut, //補間動畫
+});
+
+
+//選單
+function menu(){
+    TweenMax.staggerTo('.menu li' , 1 , {
+        x: 200
+    }, .3) //
+}
 
 
 
