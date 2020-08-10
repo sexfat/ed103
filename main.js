534a8a3199c3ba37c9af4fb8e02a9e6da1dae4d8
sexfat <bryant6831@gmail.com>
2020-08-10 10:11:34 +0800

    first

---
+++ main.js
@@ -0,0 +1,19 @@
+
+//單一物件
+TweenMax.to('.box_00', 2, {
+    //屬性
+    x: 400,
+    y: 300,
+    // width : '200px',
+    // height : '200px',
+    repeat : 2,
+    repeatDelay: 1
+    
+});
+
+//多物件
+// TweenMax.to(['.box_01', '.box_02'], 1, {
+//     x: 400
+// });
+
+
