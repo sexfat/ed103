bc7f9d67a09f8103234ca9fc5e499848a0f13b1f
sexfat <bryant6831@gmail.com>
2020-10-15 09:37:42 +0800

    add copy

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,10 @@
 var gulp = require('gulp');
 
-gulp.task('hi' ,function () {
+gulp.task('hi', function () {
     //do something
     console.log('hello world 你好gulp');
+});
+
+gulp.task('copy', function () {
+    return gulp.src('index.html').pipe(gulp.dest('html'))
 })
