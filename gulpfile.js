3bcbba1b707a5e846996491e8347e880f5a50d34
sexfat <bryant6831@gmail.com>
2020-10-15 10:34:32 +0800

    add

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,5 @@
-var gulp = require('gulp');
+const gulp = require('gulp');
+const cleanCSS = require('gulp-clean-css');
 
 gulp.task('hi', function () {
     //do something
@@ -7,4 +8,12 @@ gulp.task('hi', function () {
 
 gulp.task('copy', function () {
     return gulp.src('index.html').pipe(gulp.dest('html'))
+})
+
+
+gulp.task('minicss' ,  function(){
+  return gulp.src('style.css')
+  .pipe(cleanCSS({
+    compatibility: 'ie8'
+  })).pipe(gulp.dest('css'))
 })
