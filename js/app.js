4da3531b4b9d344dcedfd5527320bb37196b4770
sexfat <bryant6831@gmail.com>
2020-10-19 11:04:59 +0800

    add babel

--- js/app.js
+++ js/app.js
@@ -1,2 +1,12 @@
 const aa = (a , b ) => a * b;
 
+
+
+let numbers = [1, 2, 3];
+
+// callback 用 Arrow Functions 的寫法更精簡
+let doubles = numbers.map(num => {
+    return num * 2;
+});
+
+console.log(doubles);
--- js/es5/app.js
+++ js/es5/app.js
@@ -3,3 +3,10 @@
 var aa = function aa(a, b) {
   return a * b;
 };
+
+var numbers = [1, 2, 3]; // callback 用 Arrow Functions 的寫法更精簡
+
+var doubles = numbers.map(function (num) {
+  return num * 2;
+});
+console.log(doubles);
