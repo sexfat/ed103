507df783b6373da8dd92a8ecb9a22d5b01a8ef73
sexfat <bryant6831@gmail.com>
2020-10-22 14:33:34 +0800

    add

--- README.md
+++ README.md
@@ -126,5 +126,42 @@ https://webpack.js.org/plugins/mini-css-extract-plugin/
 
 
 
+```js
+
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+    entry: {
+        app: './src/index.js',
+        app2: './src/index2.js'
+    }, //兩個進入點
+    output: {
+        path: path.resolve(__dirname, 'dist'), //產出資料夾
+        filename: '[name].bundle.js' //產出檔案名稱
+    },
+    module: {
+        rules: [{
+            test: /\.css$/,
+            use: [{
+                    loader: MiniCssExtractPlugin.loader,
+                    options: {
+                        publicPath : './dist'
+                    }
+                }, //順序二
+                {
+                    loader: "css-loader"
+                } // 順序一
+            ]
+        }]
+    },
+    plugins: [
+       new MiniCssExtractPlugin({
+          filename : 'main.css' 
+       }) 
+    ]
+};
+
+```
+
 
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,34 @@
 const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 
 module.exports = {
     entry: {
-        app : './src/index.js',
-        app2 : './src/index2.js' 
+        app: './src/index.js',
+        app2: './src/index2.js'
     }, //兩個進入點
-    output : {
-       path : path.resolve(__dirname , 'dist'), //產出資料夾
-       filename : '[name].bundle.js' //產出檔案名稱
+    output: {
+        path: path.resolve(__dirname, 'dist'), //產出資料夾
+        filename: '[name].bundle.js' //產出檔案名稱
     },
     module: {
-        rules: [
-          {
+        rules: [{
             test: /\.css$/,
-            use: [
-              { loader: "style-loader" }, //順序二
-              { loader: "css-loader" } // 順序一
+            use: [{
+                    loader: MiniCssExtractPlugin.loader,
+                    options: {
+                        publicPath : './dist'
+                    }
+                }, //順序二
+                {
+                    loader: "css-loader"
+                } // 順序一
             ]
-          }
-        ]
-      }
-  };
+        }]
+    },
+    plugins: [
+       new MiniCssExtractPlugin({
+          filename : 'style.css' 
+       }) 
+    ]
+};
