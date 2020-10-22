33fb3b0bebea07342a82dfaf54cadce3fb1ae5d6
sexfat <bryant6831@gmail.com>
2020-10-22 13:46:51 +0800

    add css loader

--- src/index.js
+++ src/index.js
@@ -1,4 +1,12 @@
 import $ from "jquery";
+import TweenMax from "gsap"; 
+import "./style.css"
 
-$('.box').css('color','white');
+$('.box').css('color','black');
 console.log('webpack ok');
+
+
+
+TweenMax.to('.box' , 1 , {
+    x:200
+})
